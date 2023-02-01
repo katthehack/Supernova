512119fdb5be9b3c30f05ac4ce52379047bf40cb
katthehack <kathy2000b@gmail.com>
2023-02-01 15:29:43 -0800

    walk cycle for sol
    only forward for now; delay hates me

--- Supernova/Assets/C# Scripts/ChangeSprite.cs
+++ Supernova/Assets/C# Scripts/ChangeSprite.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
@@ -8,24 +9,33 @@ public class ChangeSprite : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite[] sprites;
+    int frame;
     // Start is called before the first frame update
     void Start()
     {
-        
+        frame = 1;
+        Boolean doesLoop = true;
+        while (doesLoop)
+        {
+            cycle();
+        }
     }
-
     // Update is called once per frame
-    void Update()
+
+    void cycle()
     {
-        
         if (Input.GetKey(KeyCode.UpArrow))
         {
             spriteRenderer.sprite = sprites[0];
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.sprite = sprites[frame];
+            frame++;
+            if (frame > 7) frame = 1;
+
         }
+        // yield return new WaitForSeconds(0.111f);
     }
-    
+
 }
