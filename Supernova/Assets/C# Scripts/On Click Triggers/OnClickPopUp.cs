72ee39a1a9f504e00c8333b92c9692a9544c1ac8
katthehack <kathy2000b@gmail.com>
2023-02-21 15:16:42 -0800

    dont touch my script

--- Supernova/Assets/C# Scripts/On Click Triggers/OnClickPopUp.cs
+++ Supernova/Assets/C# Scripts/On Click Triggers/OnClickPopUp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,26 +12,36 @@ public class OnClickPopUp : MonoBehaviour
      public TMP_Text popUpText;
     bool openOrClose;
     public string Dialog;
+    public BoxCollider2D decor;
+    public BoxCollider2D player; //finish this
     public void Start()
     {
         animator=GetComponent<Animator>();
         openOrClose= false; //false means closed
         popUpText.text = Dialog;
     }
-    
-   public void Update()
+    public void Update()
     {
-        if (animator != null)
+        
+    }
+    public void TextBoxStatus(Collision collision)
+    {
+        //object = GameObject.FindWithTag("Player").GetComponent<Collider2D>();
+        Debug.Log("button clicked");
+        if (collision.gameObject.tag == "Player"&&animator != null)
         {
+            Debug.Log("collided");
             if(!openOrClose&&Input.GetKeyDown(KeyCode.Space)) 
             {
                 animator.SetTrigger("DialogueOpen");
                 openOrClose= true;
+                Debug.Log("open");
             }
             if (openOrClose&&Input.GetKeyDown(KeyCode.Space))
             {
                 animator.SetTrigger("DialogueClose");
                 openOrClose= false;
+                Debug.Log("close");
             }
 
         }
