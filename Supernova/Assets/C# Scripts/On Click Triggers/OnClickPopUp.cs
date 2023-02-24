4853bf9a62b09ec1f812032859729150c977d785
katthehack <kathy2000b@gmail.com>
2023-02-23 23:02:05 -0800

    text box pain

--- Supernova/Assets/C# Scripts/On Click Triggers/OnClickPopUp.cs
+++ Supernova/Assets/C# Scripts/On Click Triggers/OnClickPopUp.cs
@@ -13,33 +13,43 @@ public class OnClickPopUp : MonoBehaviour
     bool openOrClose;
     public string Dialog;
     public BoxCollider2D decor;
-    public BoxCollider2D player; 
+    public BoxCollider2D player;
+    KeyCode input = KeyCode.E;
+    public GameObject animObject;
     public void Start()
     {
-        animator=GetComponent<Animator>();
-        openOrClose= false; //false means closed
+        animator=animObject.GetComponent<Animator>();
+        openOrClose= animator.GetBool("OpenOrClose"); //false means closed
         popUpText.text = Dialog;
     }
     public void Update()
     {
+        
         decor = GameObject.FindWithTag("Decor").GetComponent<BoxCollider2D>(); //decor-any object that will be interactible
         player = GameObject.FindWithTag("Player").GetComponent<BoxCollider2D>();
-        Debug.Log("button clicked");
         if (decor.IsTouching(player)&&animator != null)
         {
             Debug.Log("collided");
-            if(!openOrClose&&Input.GetKeyDown(KeyCode.E)) 
+            if(!openOrClose&&Input.GetKeyDown(input)) 
             {
+               // animator.Play("text box open");
                 animator.SetTrigger("DialogueOpen");
                 openOrClose= true;
+                animator.SetBool("OpenOrClose",true); //bool not updating, open anim not playing while close plays
+                
                 Debug.Log("open");
             }
-            if (openOrClose&&Input.GetKeyDown(KeyCode.E))
+            else if (openOrClose&&Input.GetKeyDown(input))
             {
                 animator.SetTrigger("DialogueClose");
                 openOrClose= false;
+                animator.SetBool("OpenOrClose", false);
+               // animator.Play("text box close");
+              //  animator.Play("text box idle");
                 Debug.Log("close");
+                //alt method is commented out
             }
+        
 
         }
 
