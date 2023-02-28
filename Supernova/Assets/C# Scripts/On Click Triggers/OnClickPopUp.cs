0cca14568d5de8e4b09f3bd24b261305cf8569bb
katthehack <kathy2000b@gmail.com>
2023-02-27 18:24:22 -0800

    fixing
    fixed text box not working for only one dialog

--- Supernova/Assets/C# Scripts/On Click Triggers/OnClickPopUp.cs
+++ Supernova/Assets/C# Scripts/On Click Triggers/OnClickPopUp.cs
@@ -6,11 +6,9 @@ using UnityEngine;
 
 public class OnClickPopUp : MonoBehaviour
 {
-    // public GameObject popUpBox;
-  //  public Rigidbody2D rb;
+  
    public Animator animator;
-     public TMP_Text popUpText;
- //   bool openOrClose;
+    public TMP_Text popUpText;
     public string [] Dialog;
     public int repeats;
     public BoxCollider2D decor;
@@ -21,67 +19,67 @@ public class OnClickPopUp : MonoBehaviour
     public void Start()
     {
         animator=animObject.GetComponent<Animator>();
-  //      openOrClose= animator.GetBool("OpenOrClose"); //false means closed
+  
         repeats = 0;
     }
     public void Update()
     {
-        animator.SetBool("EndOfDialogue", false);
+       
             decor = GameObject.FindWithTag("Decor").GetComponent<BoxCollider2D>(); //decor-any object that will be interactible
             player = GameObject.FindWithTag("Player").GetComponent<BoxCollider2D>();
-            if (decor.IsTouching(player) && animator != null)
+            if (decor.IsTouching(player) && animator != null&&Dialog.Length>1)
             {
+            
                 Debug.Log("collided");
                 if (repeats==0&&Input.GetKeyDown(input))
                 {
                 popUpText.text = Dialog[repeats];
                 animator.Play("text box open");
-                animator.SetTrigger("DialogueOpen");
-                    animator.ResetTrigger("DialogueClose");
-               //     openOrClose = true;
-                    animator.SetBool("OpenOrClose", true); //bool not updating, open anim not playing while close plays
-                animator.SetBool("EndOfDialogue", false);
-                Debug.Log("open");
+               
                     repeats++;
                 }
                 else if (repeats>0&&Input.GetKeyDown(input)&&!lastDialogue)
                 {
                 popUpText.text = Dialog[repeats];
-                animator.SetTrigger("DialogueClose");
-                    //  animator.ResetTrigger("DialogueOpen");
-                 //   openOrClose = false;
-                    animator.SetBool("OpenOrClose", false);
-                    // animator.Play("text box close");
-                    //  animator.Play("text box idle");
-                    Debug.Log("close");
-                //alt method is commented out
+              
                 if (repeats != Dialog.Length - 1) repeats++;
                 else lastDialogue = true;
                 
                 }
-            else if (Input.GetKeyDown(input)&&lastDialogue)
+                else if (Input.GetKeyDown(input)&&lastDialogue)
+                {
+                     animator.Play("text box close");
+                    Debug.Log("close");
+                    repeats=0;
+                    lastDialogue = false;
+                }
+
+
+            }
+
+        else if (decor.IsTouching(player) && animator != null && Dialog.Length ==1)
+        {
+
+            Debug.Log("collided");
+            if (Input.GetKeyDown(input)&&!lastDialogue)
             {
-                animator.SetTrigger("DialogueClose");
-                 animator.ResetTrigger("DialogueOpen");
-              //  openOrClose = false;
-                animator.SetBool("OpenOrClose", false);
-                animator.SetBool("EndOfDialogue", true);
-                 animator.Play("text box close");
-                 animator.Play("text box idle");
+                popUpText.text = Dialog[repeats];
+                animator.Play("text box open");
+                Debug.Log("open");
+                lastDialogue = true;
+            }
+           
+            else if (Input.GetKeyDown(input) && lastDialogue)
+            {
+                animator.Play("text box close");
                 Debug.Log("close");
-                //alt method is commented out
-                repeats=0;
+                repeats = 0;
                 lastDialogue = false;
             }
 
 
-            }
-
-            
-          
-
-            //  back up plan uses this: if (collision.gameObject.tag == "Player") {}
-            //requires passing in collision, which you cant do in an update function; would need button, so trying to avoid that here
         }
+
+    }
 
 }
